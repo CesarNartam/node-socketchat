3d060cfadc93fabf42df0127589a23dab271efd8
Sirsen <213473@ids.upchiapas.edu.mx>
2023-01-27 01:15:23 -0600

    username input and localhost added

--- client.js
+++ client.js
@@ -6,20 +6,58 @@ const readline = require('readline').createInterface({
 
 const END = 'END';
 
-const socket = new Socket();
+const error = (message) => {
+    console.log(message);
+    process.exit(1);
+};
 
-socket.connect({host: 'localhost', port: 8000 });
-socket.setEncoding('utf-8');
-
-readline.on("line", (message) => {
-    socket.write(message);
-    if (message === END) {
-        socket.end();
-    }
+const connect = (host, port) => {
+    console.log(`Connecting to ${host}:${port}`);
     
-});
-socket.on("data", (data) => {
-    console.log(data);
-});
+    const socket = new Socket();
+    socket.connect({ host, port });
+    socket.setEncoding('utf-8');
+    
+    socket.on("connect", () => {
+        console.log('Connected');
 
-socket.on("close", () => process.exit(0));
+        readline.question("Choose your username: ", (username) => {
+            socket.write(username);
+            console.log(`Type any message to send a message, type ${END} to finish your session`);
+        })
+
+        readline.on("line", (message) => {
+            socket.write(message);
+            if (message === END) {
+                socket.end();
+                console.log("Disconnected");
+            }
+    
+        });
+        socket.on("data", (data) => {
+            console.log(data);
+        });
+    });
+    
+
+    socket.on("error", (err) => error(err.message));
+    socket.on("close", () => process.exit(0));
+};
+
+const main = () => {
+    if (process.argv.length !== 4) {
+        error(`Usage: node ${__filename} host port`);
+    }
+
+    let [, , host, port] = process.argv;
+    if (isNaN(port)) {
+        error(`Invalid port ${port}`);
+    }
+    port = Number(port);
+    
+    connect(host, port);
+}
+
+if ( module === require.main ) {
+     main(); 
+}
--- server.js
+++ server.js
@@ -1,27 +1,71 @@
 const { Server } = require('net');
 
-const server = new Server();
-
+const host = "0.0.0.0";
 const END = 'END';
 
-server.on("connection", (socket) => {
-    const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`;
-    console.log(`New connection from ${remoteSocket}`);
-    socket.setEncoding('utf-8');
-    socket.on("data", (message) => {
-        if (message === END) {
-            socket.end();
-        }
-        else {
-            console.log(`${remoteSocket} -> ${message}`);
-        }
+const connections = new Map();
+
+const error = (message) => {
+    console.log(message);
+    process.exit(1);
+};
+
+const sendMessage = (message, origin) => {
+     
+};
+
+const listen = (port) => {
+    const server = new Server();
+
+    server.on("connection", (socket) => {
+        const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`;
+        
+        console.log(`New connection from ${remoteSocket}`);
+        socket.setEncoding('utf-8');
+
+        socket.on("data", (message) => {
+            if (!connections.has(socket)) {
+                console.log(`Username ${message} set for connection ${remoteSocket}`)
+                connections.set(socket, message);
+            }
+            else if (message === END) {
+                socket.end();
+            }
+            else {
+                console.log(`${remoteSocket} -> ${message}`);
+            }
+        });
+
+        socket.on("error", (err) => error(err.message));
+    
+        socket.on("close", () => {
+            console.log(`Connection with ${remoteSocket} closed`);
+        });
+    });
+    
+    server.listen({ port, host }, () => {
+        console.log("Listening on port 8000");
     });
 
-    socket.on("close", () => {
-        console.log(`Connection with ${remoteSocket} closed`);
-    })
-});
+    server.on("error", (err) => error(err.message));
+};
 
-server.listen({ port: 8000, host: '0.0.0.0' }, () => {
-    console.log("Listening on port 8000");
-});
+const main = () => {
+    if (process.argv.length !== 3) {
+        error(`Usage: node ${__filename} port`);
+    }
+
+    let port = process.argv[2]
+    if (isNaN(port)) {
+        error(`Invalid port ${port}`)
+    }
+
+    port = Number(port);
+
+    listen(port);
+}
+
+if (require.main === module) {
+    main();
+}
+
